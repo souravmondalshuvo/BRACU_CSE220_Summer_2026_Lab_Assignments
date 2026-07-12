53031ed59d3d64fd5193e0c09318c68df5eeb187
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-07-12 14:45:35 +0600

    CSE220 Summer 2026 Lab Assignments 2 (Part 2) Files Updated

---
+++ CSE220 Summer 2026 Lab Assignments 2/CSE220 Summer 2026 Lab Assignments 2 (Part 2)/Lab Task/WRM.java
@@ -0,0 +1,38 @@
+// NO NEED THIS SUBMIT THIS CLASS
+public class WRM {
+    Patient dh;
+
+    //The constructor is already created for you
+    public WRM() {
+        dh = new Patient(null, null, null, null, null, null);
+        dh.next = dh;
+        dh.prev = dh;
+    }
+
+    public void registerPatient(int id, String name, int age, String bloodgroup) {
+        // To Do
+    }
+
+    public void servePatient() {
+        // To do
+    }
+
+    public void showAllPatient() {
+        // To Do
+    }
+
+    public Boolean canDoctorGoHome() {
+        // To Do
+        return null; // Delete this line once you're ready
+    }
+
+    public void cancelAll() {
+        // To Do
+    }
+
+
+    public void reverseTheLine() {
+        // To Do
+    }
+
+}
