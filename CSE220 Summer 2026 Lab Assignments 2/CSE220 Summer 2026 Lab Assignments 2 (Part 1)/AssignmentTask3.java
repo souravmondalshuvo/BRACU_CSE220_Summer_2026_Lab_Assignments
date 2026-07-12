5f66e1ee4c60d56effa3888e5eae3577812c8508
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-07-12 13:41:08 +0600

    CSE220 Summer 2026 Lab Assignments 2 (Part 1) AssignmentTask3 Completed

---
+++ CSE220 Summer 2026 Lab Assignments 2/CSE220 Summer 2026 Lab Assignments 2 (Part 1)/AssignmentTask3.java
@@ -0,0 +1,75 @@
+// AssignmentTask3: Alternate Merge
+public class AssignmentTask3 {
+    
+    // MUST SUBMIT this method
+    public static Node alternateMerge( Node head1, Node head2 ){
+        
+        //You’re NOT ALLOWED to create a new singly linked list for this task
+
+        //TO DO
+
+        Node walk1 = head1;
+        Node walk2 = head2;
+
+        while(walk1 != null && walk2 != null) {
+            Node rest1 = walk1.next;
+            Node rest2 = walk2.next;
+
+            walk1.next = walk2;
+
+            if(rest1 != null) {
+                walk2.next = rest1;
+            }
+
+            walk1 = rest1;
+            walk2 = rest2;
+        }
+
+        //remove the following line when returning the Head of the modified LinkedList
+        // return null;
+        return head1;
+    }
+
+    //NOTE: if you find any issue with the driver code please inform AIB
+    //DO NOT TOUCH THE DRIVER CODE BELOW
+    public static void main(String[] args){
+        System.out.println("==============Test Case 1=============");
+        Node head1 = LinkedList.createList(new Integer[]{1,2,6,8,11});
+        Node head2 = LinkedList.createList(new Integer[]{5,7,3,9,4});
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head1);
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head2);
+        Node result = alternateMerge(head1, head2);
+        System.out.println("\nExpected output: 1 -> 5 -> 2 -> 7 -> 6 -> 3 -> 8 -> 9 -> 11 -> 4");
+        System.out.print( "Your output: ");
+        LinkedList.printLL(result); //This should print 1 -> 5 -> 2 -> 7 -> 6 -> 3 -> 8 -> 9 -> 11 -> 4
+        System.out.println();
+        
+        System.out.println("\n==============Test Case 2=============");
+        head1 = LinkedList.createList(new Integer[]{5,3,2,-4});
+        head2 = LinkedList.createList(new Integer[]{-4,-6,1});
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head1);
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head2);
+        result = alternateMerge(head1, head2);
+        System.out.println("\nExpected output: 5 -> -4 -> 3 -> -6 -> 2 -> 1 -> -4");
+        System.out.print( "Your output: ");
+        LinkedList.printLL(result); //This should print 5 -> -4 -> 3 -> -6 -> 2 -> 1 -> -4
+        System.out.println();
+
+        System.out.println("\n==============Test Case 3=============");
+        head1 = LinkedList.createList(new Integer[]{4,2,-2,-4});
+        head2 = LinkedList.createList(new Integer[]{8,6,5,-3});
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head1);
+        System.out.print("Linked List 1: ");
+        LinkedList.printLL(head2);
+        result = alternateMerge(head1, head2);
+        System.out.println("\nExpected output: 4 -> 8 -> 2 -> 6 -> -2 -> 5 -> -4 -> -3");
+        System.out.print( "Your output: ");
+        LinkedList.printLL(result); //This should print 4 -> 8 -> 2 -> 6 -> -2 -> 5 -> -4 -> -3
+        System.out.println();
+    }
+}
