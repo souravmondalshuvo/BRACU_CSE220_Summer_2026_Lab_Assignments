dcab9d063893ebef78fb521678c4bbe0101e3ee1
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-07-12 15:11:34 +0600

    CSE220 Summer 2026 Lab Assignments 2 (Part 2) Files Updated

---
+++ CSE220 Summer 2026 Lab Assignments 2/CSE220 Summer 2026 Lab Assignments 2 (Part 2)/Assignment Tasks/AssignmentTask6.java
@@ -0,0 +1,34 @@
+public class AssignmentTask6 {
+
+    //SUBMIT ONLY THIS METHOD
+    public static void pairJoin(DNode dh1, DNode dh2) {
+        // TO DO
+    }
+
+    //DO NOT SUBMIT THE DRIVER CODE BELOW
+    //SUBMITTING IT WILL INCREASE YOUR PLAG % FOR NO REASON
+    public static void main(String[] args) {
+        Object[] list1 = {"A", "B", "C", "D"};
+        Object[] list2 = {"E", "F", "G", "H"};
+
+        DNode dh1 = LinkedListHelpers.createDummyHeadedDoublyLL(list1, false);
+        DNode dh2 = LinkedListHelpers.createDummyHeadedDoublyLL(list2, false);
+
+        System.out.println("Given Linked List 1:");
+        LinkedListHelpers.printDummyHeadedDoublyLL(dh1);
+
+        System.out.println("Given Linked List 2:");
+        LinkedListHelpers.printDummyHeadedDoublyLL(dh2);
+
+        System.out.println("\nExpected Output:");
+        Object[] expected = {"A", "E", "B", "F", "C", "G", "D", "H"};
+        DNode expectedHead = LinkedListHelpers.createDummyHeadedDoublyLL(expected, true);
+        LinkedListHelpers.printDummyHeadedDoublyCircularLL(expectedHead);
+
+        //Running the Pair Join
+        pairJoin(dh1, dh2);
+        //Printing after Pair Join
+        System.out.println("\nYour Output:");
+        LinkedListHelpers.printDummyHeadedDoublyCircularLL(dh1);
+    }
+}
