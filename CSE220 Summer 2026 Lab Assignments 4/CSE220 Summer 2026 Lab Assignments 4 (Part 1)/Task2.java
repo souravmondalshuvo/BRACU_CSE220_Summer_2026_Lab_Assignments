6a18fd28f9bd8033c496cf26561b05b8626771d1
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-08-23 01:57:23 +0600

    CSE220 Summer 2026 Lab Assignments 4 (Part 1) Task#2 Completed

--- CSE220 Summer 2026 Lab Assignments 4/CSE220 Summer 2026 Lab Assignments 4 (Part 1)/Task2.java
+++ CSE220 Summer 2026 Lab Assignments 4/CSE220 Summer 2026 Lab Assignments 4 (Part 1)/Task2.java
@@ -6,7 +6,13 @@ public class Task2 {
     // This method return total count of the nodes in the tree
     public static int countNodes( BTNode root ){
         // TO DO
-        return -1; // Remove this line once ready
+
+        if(root == null) {
+            return 0;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+        // return -1; // Remove this line once ready
     }
     //============================================================================
 
