f0b24d138fd2ad66c3230c3caae30af2733d09fc
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-09-06 19:03:22 +0600

    CSE220 Summer 2026 Lab Assignments 6 'Task1Tester' completed

--- CSE220 Summer 2026 Lab Assignments 6/Task1Tester.java
+++ CSE220 Summer 2026 Lab Assignments 6/Task1Tester.java
@@ -2,10 +2,15 @@ public class Task1Tester {
 
     public static void main(String[] args) {
 
-        System.out.println("Task 1a:");
-        GraphData.printAnswer("Task 1a", Task1.Task1a(GraphData.undirectedUnweightedMatrix()), "degree");
+        GraphData.header("===== Task 1a: Maximum Degree (Matrix) =====");
+        GraphData.checkAnswer("Task 1a", Task1.Task1a(GraphData.undirectedUnweightedMatrix()), "degree", 3, 4);
 
-        System.out.println("\nTask 1b:");
-        GraphData.printAnswer("Task 1b", Task1.Task1b(GraphData.undirectedUnweightedList()), "degree");
+        GraphData.header("===== Task 1b: Maximum Degree (List) =====");
+        GraphData.checkAnswer("Task 1b", Task1.Task1b(GraphData.undirectedUnweightedList()), "degree", 3, 4);
+
+        GraphData.header("===== Task 1: Representation Agreement =====");
+        int fromMatrix = Task1.Task1a(GraphData.undirectedUnweightedMatrix())[1];
+        int fromList = Task1.Task1b(GraphData.undirectedUnweightedList())[1];
+        GraphData.checkValue("Matrix and list report the same degree", fromMatrix, fromList);
     }
 }
