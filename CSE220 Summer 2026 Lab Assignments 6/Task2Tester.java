035a33b69f72c67ee9769d76fb0bc152557179f1
Sourav Mondal Shuvo <souravmondalshuvo@users.noreply.github.com>
2026-09-06 19:03:51 +0600

    CSE220 Summer 2026 Lab Assignments 6 'Task2Tester' completed

--- CSE220 Summer 2026 Lab Assignments 6/Task2Tester.java
+++ CSE220 Summer 2026 Lab Assignments 6/Task2Tester.java
@@ -2,10 +2,15 @@ public class Task2Tester {
 
     public static void main(String[] args) {
 
-        System.out.println("Task 2a:");
-        GraphData.printAnswer("Task 2a", Task2.Task2a(GraphData.undirectedWeightedMatrix()), "sum of edge weights");
+        GraphData.header("===== Task 2a: Max Weight Sum (Matrix) =====");
+        GraphData.checkAnswer("Task 2a", Task2.Task2a(GraphData.undirectedWeightedMatrix()), "sum of edge weights", 3, 19);
 
-        System.out.println("\nTask 2b:");
-        GraphData.printAnswer("Task 2b", Task2.Task2b(GraphData.undirectedWeightedList()), "sum of edge weights");
+        GraphData.header("===== Task 2b: Max Weight Sum (List) =====");
+        GraphData.checkAnswer("Task 2b", Task2.Task2b(GraphData.undirectedWeightedList()), "sum of edge weights", 3, 19);
+
+        GraphData.header("===== Task 2: Representation Agreement =====");
+        int fromMatrix = Task2.Task2a(GraphData.undirectedWeightedMatrix())[1];
+        int fromList = Task2.Task2b(GraphData.undirectedWeightedList())[1];
+        GraphData.checkValue("Matrix and list report the same sum", fromMatrix, fromList);
     }
 }
